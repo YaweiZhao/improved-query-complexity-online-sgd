8e3f06ca55c1b7c42345359e1bc5875ee7d237d8
Yawei Zhao <zhaoyawei@nudt.edu.cn>
2018-04-15 20:42:39 -0400

    dd
    dd

--- online_optimization.m
+++ online_optimization.m
@@ -7,7 +7,7 @@ elseif strcmp(model_opt,'logistic_regression')
     % do nothing
 end
 x_t = zeros(d,1);
-eta = 1e-8;%learning rate
+eta = 1e-10;%learning rate
 gamma = 1e1;%regularization constant
 %record local minimizers
 x_seq = zeros(T,d);
@@ -31,12 +31,12 @@ for i=1:T %n >> T
         if strcmp(ALGO, 'MOGD')
             delta = 2;
             eta2 = eta*delta;
-            for j = 1:fix(100) % K: iterate n/10 for GD
+            for j = 1:fix(1000) % K: iterate n/10 for GD
                 gradient = query_gradient(x_t, Ai, yi, gamma,  model_opt);
                 x_t = x_t - eta2*gradient;
             end
         elseif strcmp(ALGO, 'OMGD')
-            for j = 1:fix(100) % K: iterate n/10 for GD
+            for j = 1:fix(1000) % K: iterate n/10 for GD
                 eta2 = eta;
                 gradient = query_gradient(x_t, Ai, yi, gamma,  model_opt);
                 x_t = x_t - eta2*gradient;
