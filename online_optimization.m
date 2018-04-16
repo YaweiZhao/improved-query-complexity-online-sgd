f9c5e29d419400b91e0fe8b84df13de1e3378a15
Yawei Zhao <zhaoyawei@nudt.edu.cn>
2018-04-15 20:59:38 -0400

    dd
    dd

--- online_optimization.m
+++ online_optimization.m
@@ -7,7 +7,7 @@ elseif strcmp(model_opt,'logistic_regression')
     % do nothing
 end
 x_t = zeros(d,1);
-eta = 1e-10;%learning rate
+eta = 1e-12;%learning rate
 gamma = 1e-4;%regularization constant
 %record local minimizers
 x_seq = zeros(T,d);
