0182e0d92b94cd26c1dc6dcf000d344bde080ed6
Yawei Zhao <zhaoyawei@nudt.edu.cn>
2018-04-15 21:04:37 -0400

    dd
    dd

--- online_optimization.m
+++ online_optimization.m
@@ -29,7 +29,7 @@ for i=1:T %n >> T
     %optimization modular
     if strcmp(modular, 'GD')
         if strcmp(ALGO, 'MOGD')
-            delta = 1.5;
+            delta = 5;
             eta2 = eta*delta;
             for j = 1:fix(1000) % K: iterate n/10 for GD
                 gradient = query_gradient(x_t, Ai, yi, gamma,  model_opt);
