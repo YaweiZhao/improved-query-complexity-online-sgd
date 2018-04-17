4bbaaeb2b592a84b1f2d87575378029a51ff6f83
Yawei Zhao <zhaoyawei@nudt.edu.cn>
2018-04-17 10:49:57 -0400

    dd
    dd

--- test.m
+++ test.m
@@ -10,9 +10,9 @@ module = 'NAGM';
 dynamic_variation_base = 0.01;
 n_dynamic = 10;%10 in default
 %other settings
-n = 5000; %divide n_dynamic
+n = 500000; %divide n_dynamic
 d = 5;%5
-T = 5000; 
+T = 500000; 
 
 [A, y] = generate_dynamic_data_stream(n, d, model_opt,dynamic_variation_base,n_dynamic);
 
