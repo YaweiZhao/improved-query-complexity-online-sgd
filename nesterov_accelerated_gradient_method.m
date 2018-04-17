9b30ed45efdf05497fa7b4481f6f56604ced5837
Yawei Zhao <zhaoyawei@nudt.edu.cn>
2018-04-17 17:17:56 -0400

    dd
    dd

--- nesterov_accelerated_gradient_method.m
+++ nesterov_accelerated_gradient_method.m
@@ -12,7 +12,7 @@ clear options;
 % general options for optimization algorithms
 options.w_init = x_t;
 options.tol_gnorm = 1e-6;
-options.max_iter = 1000;
+options.max_iter = 100;
 options.verbose = false;
 options.store_w = false;
 options.step_alg = 'backtracking';
