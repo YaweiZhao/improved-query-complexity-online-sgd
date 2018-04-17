aa4215e4ab3204a431170b863d1067676fcd17d5
Yawei Zhao <zhaoyawei@nudt.edu.cn>
2018-04-17 17:59:46 -0400

    dd
    dd

--- logistic_regression_figures/draw_figures_dynamic_regret_algos_logistic_regression.m
+++ logistic_regression_figures/draw_figures_dynamic_regret_algos_logistic_regression.m
@@ -7,33 +7,36 @@ sum_time_seq_OGD = load('./OGD/sum_time_seq.txt','-ascii');
 sum_f_seq_OMGD = load('./OMGD/sum_f_seq.txt','-ascii');
 sum_time_seq_OMGD = load('./OMGD/sum_time_seq.txt','-ascii');
 sum_f_seq_MOGD = load('./MOGD/sum_f_seq.txt','-ascii');
+    %sum_f_seq_MOGD = [sum_f_seq_MOGD; sum_f_seq_MOGD(length(sum_f_seq_MOGD))*ones(2,1)];
 sum_time_seq_MOGD = load('./MOGD/sum_time_seq.txt','-ascii');
 sum_f_seq_NAGM = load('./NAGM/sum_f_seq.txt','-ascii');
+    %sum_f_seq_NAGM = [sum_f_seq_NAGM; sum_f_seq_NAGM(length(sum_f_seq_NAGM))*ones(11,1)];
 sum_time_seq_NAGM = load('./NAGM/sum_time_seq.txt','-ascii');
 
 model_opt = 'logistic_regression';
 if strcmp(model_opt,'logistic_regression')
     
     %var = 0.01
-    semilogy(sum_time_seq_OGD, sum_f_seq_OGD, '-r'); 
+    
+    semilogy(sum_time_seq_OGD, sum_f_seq_OGD, '-r','LineWidth', 2); 
     hold on;
-    semilogy(sum_time_seq_OMGD, sum_f_seq_OMGD, '-b'); 
+    semilogy(sum_time_seq_OMGD, sum_f_seq_OMGD, '--b','LineWidth', 2); 
     hold on;
     
-    semilogy(sum_time_seq_MOGD, sum_f_seq_MOGD, '-g');
+    semilogy(sum_time_seq_MOGD, sum_f_seq_MOGD, '-.g','LineWidth', 2);
     hold on;
-    semilogy(sum_time_seq_NAGM, sum_f_seq_NAGM, '-k'); 
+    semilogy(sum_time_seq_NAGM, sum_f_seq_NAGM, ':k','LineWidth', 2); 
     hold on;
     
     
-    xlabel('Iterations', 'fontsize', 18);
+    xlabel('CPU seconds', 'fontsize', 18);
     ylabel('Dynamic regret', 'fontsize', 18 );
-    axis([1 400 1 10000]);
+    axis([1 400 1e-6 5e2]);
     %$$ \mathcal{P}_T^{\ast} $$
     h = legend('OGD', ...
         'OMGD', ...
-        'MOGD', ...
-        'NAGM', ...
+        'MOGD-GD', ...
+        'MOGD-NAGM', ...
         'Location', 'southeast');
     
     set(h,'Interpreter','latex');
