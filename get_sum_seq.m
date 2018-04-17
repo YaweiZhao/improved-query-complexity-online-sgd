af97861726c2596ba14fd5403d3077f1cfc8c5e4
Yawei Zhao <zhaoyawei@nudt.edu.cn>
2018-04-17 13:36:39 -0400

    dd
    dd

--- get_sum_seq.m
+++ get_sum_seq.m
@@ -14,17 +14,6 @@ for i=1:n_x
     regret_squared_path_seq(i,:) = sum(diff_x_seq(1:i) .* diff_x_seq(1:i));
 end
 
-% regret accumulated sum
-diff_f_seq = f_t_seq - f_seq;
-nonzero_index = find(diff_f_seq);
-last_nonzero_index_diff_f = nonzero_index(length(nonzero_index));
-diff_f_seq = diff_f_seq(1:last_nonzero_index_diff_f);
-n_f = length(diff_f_seq);
-regret_f_seq = zeros(n_f, 1);
-for i=1:n_f
-    regret_f_seq(i,:) = sum(diff_f_seq(1:i));
-end
-
 % time
 nonzero_index = find(time_seq);
 last_nonzero_index_time_seq = nonzero_index(length(nonzero_index));
@@ -34,6 +23,21 @@ sum_time_seq = zeros(n_t,1);
 for i=1:n_t
     sum_time_seq(i,:) = sum(time_seq(1:i));
 end
+
+% regret accumulated sum
+diff_f_seq = f_t_seq - f_seq;
+nonzero_index = find(diff_f_seq);
+last_nonzero_index_diff_f = nonzero_index(length(nonzero_index));
+diff_f_seq = diff_f_seq(1:last_nonzero_index_diff_f);
+n_f = length(diff_f_seq);
+regret_f_seq = zeros(max(n_f, n_t), 1);
+for i=1:n_f
+    regret_f_seq(i,:) = sum(diff_f_seq(1:i));
+end
+
+
+
+
 
 end
 
--- ridge_regression_figures/draw_figures_dynamic_regret_algos_ridge_regression.m
+++ ridge_regression_figures/draw_figures_dynamic_regret_algos_ridge_regression.m
@@ -7,14 +7,17 @@ sum_time_seq_OGD = load('./OGD/sum_time_seq.txt','-ascii');
 sum_f_seq_OMGD = load('./OMGD/sum_f_seq.txt','-ascii');
 sum_time_seq_OMGD = load('./OMGD/sum_time_seq.txt','-ascii');
 sum_f_seq_MOGD = load('./MOGD/sum_f_seq.txt','-ascii');
+    %sum_f_seq_MOGD = [sum_f_seq_MOGD; sum_f_seq_MOGD(length(sum_f_seq_MOGD))*ones(2,1)];
 sum_time_seq_MOGD = load('./MOGD/sum_time_seq.txt','-ascii');
 sum_f_seq_NAGM = load('./NAGM/sum_f_seq.txt','-ascii');
+    %sum_f_seq_NAGM = [sum_f_seq_NAGM; sum_f_seq_NAGM(length(sum_f_seq_NAGM))*ones(11,1)];
 sum_time_seq_NAGM = load('./NAGM/sum_time_seq.txt','-ascii');
 
 model_opt = 'ridge_regression';
 if strcmp(model_opt,'ridge_regression')
     
     %var = 0.01
+    
     semilogy(sum_time_seq_OGD, sum_f_seq_OGD, '-r','LineWidth', 2); 
     hold on;
     semilogy(sum_time_seq_OMGD, sum_f_seq_OMGD, '--b','LineWidth', 2); 
@@ -28,7 +31,7 @@ if strcmp(model_opt,'ridge_regression')
     
     xlabel('CPU seconds', 'fontsize', 18);
     ylabel('Dynamic regret', 'fontsize', 18 );
-    axis([1 400 1e-2 100000]);
+    axis([1 400 1e-4 1e4]);
     %$$ \mathcal{P}_T^{\ast} $$
     h = legend('OGD', ...
         'OMGD', ...
