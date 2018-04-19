9a752842f609c4a1398829deb9367636ccf8aa5b
Yawei Zhao <zhaoyawei@nudt.edu.cn>
2018-04-18 22:12:28 -0400

    d
    d

--- test.m
+++ test.m
@@ -3,10 +3,10 @@ clc;
 
 rng('default');
 %settings of dynamic environment
-%model_opt = 'ridge_regression';
-model_opt = 'logistic_regression';
-module = 'GD';
-%module = 'NAGM';
+model_opt = 'ridge_regression';
+%model_opt = 'logistic_regression';
+%module = 'GD';
+module = 'NAGM';
 dynamic_variation_base = 0.01;
 n_dynamic = 10;%10 in default
 %other settings
