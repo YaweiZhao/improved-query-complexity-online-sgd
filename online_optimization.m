66f73b96415d87200a884673cbfc0138746adbd4
Yawei Zhao <zhaoyawei@nudt.edu.cn>
2018-04-27 17:32:35 -0400

    d
    d

--- online_optimization.m
+++ online_optimization.m
@@ -113,7 +113,7 @@ for i=1:T %n >> T
     
     %terminate the process
     cpu_seconds = cpu_seconds + time_seq(i,:);
-    if cpu_seconds > 306
+    if cpu_seconds > 36
         break;
     end
 end
