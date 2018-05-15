0182eb279431253742cedf94e342511a1ef91df4
Yawei Zhao <zhaoyawei@nudt.edu.cn>
2018-05-15 19:42:07 -0400

    d
    d

--- online_optimization.m
+++ online_optimization.m
@@ -116,7 +116,7 @@ for i=1:T %n >> T
     %[x_seq(i,:), f_seq(i,:)] = get_local_minimizer(x_t, Ai, [],  s_hyp) ;
     %f_t_seq(i,:) = get_local_loss(x_t, Ai, [],  s_hyp) ;
     x_seq(i,:) = x_t';
-    interval=50;
+    interval=100;
     if strcmp(ALGO, 'MOGD10') || strcmp(ALGO, 'OMGD')
         if mod(i,interval) == 0
             counter = fix(i/interval);
