fae523549338666448f7cb8e7e4d6c496ec1acb4
Yawei Zhao <zhaoyawei@nudt.edu.cn>
2018-05-16 09:36:17 -0400

    dd
    dd

--- result/logistic regression/regret/scale=1e0,kappa=250/draw_figures_dynamic_regret_algos_logistic_regression_kappa_250.m
+++ result/logistic regression/regret/scale=1e0,kappa=250/draw_figures_dynamic_regret_algos_logistic_regression_kappa_250.m
@@ -1,17 +1,17 @@
-%scale = 1e2
+%scale = 1e0
 
 
 
 model_opt = 'logistic_regression';
 if strcmp(model_opt,'logistic_regression')
-    sum_time_seq_OGD = 100:100:800;
-    sum_f_seq_OGD = [0.325 1.807 5.268 11.565 21.09 33.23 49 67];
+    sum_time_seq_OGD = [100 200 300 400 500 700 800 900];
+    sum_f_seq_OGD = [ 0.0312 0.034 0.036 0.038 0.0388 0.04 0.042 0.0426];
     
-    sum_time_seq_OMGD = 100:100:800;
-    sum_f_seq_OMGD = [0.3196 1.749 5.01 10.8 19.29 29.71 42.68 56.8 ];
+    sum_time_seq_OMGD = [100 200 300 400 500 700 800 900];
+    sum_f_seq_OMGD = [0.0062 0.0068 0.0071 0.0076 0.00784 0.0084 0.0087 0.0089];
     
-    sum_time_seq_MOGD = 100:100:800;
-    sum_f_seq_MOGD = [0.294 1.487 3.94 7.8 12.7 18.12 25.4 33.89 ];
+    sum_time_seq_MOGD = [100 200 300 400 500 700 800 900];
+    sum_f_seq_MOGD = [0.001 0.0013 0.0014 0.0016 0.00172 0.002 0.00215 0.0023];
     
     semilogy(sum_time_seq_OGD, sum_f_seq_OGD, '-r','LineWidth', 2);
     hold on;
@@ -27,7 +27,7 @@ if strcmp(model_opt,'logistic_regression')
     ylabel('Dynamic regret', 'fontsize', 18 );
     %set(gca,'ytick',[1e-9 1e-7 1e-5 1e-3 1e-1 1e1 1e3]);
     %set(gca,'yticklabel',{'Two','Four','Five','Seven'});
-    axis([100 800 0.2 100]);
+    axis([100 900 1e-4 1e-1]);
     %$$ \mathcal{P}_T^{\ast} $$
     h = legend('OGD', ...
         'OMGD', ...
