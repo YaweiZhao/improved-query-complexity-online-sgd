e352dd72b1f2de1eff28adc014d7cbb8d3bc972c
Yawei Zhao <zhaoyawei@nudt.edu.cn>
2018-05-16 17:03:46 -0400

    dd
    dd

--- result/logistic regression/regret/scale=1e2,kappa=250/draw_figures_dynamic_regret_algos_logistic_regression_kappa_250.m
+++ result/logistic regression/regret/scale=1e2,kappa=250/draw_figures_dynamic_regret_algos_logistic_regression_kappa_250.m
@@ -1,17 +1,33 @@
-%scale = 1e2
-
 
 
 model_opt = 'logistic_regression';
 if strcmp(model_opt,'logistic_regression')
-    sum_time_seq_OGD = 100:100:1000;
-    sum_f_seq_OGD = [0.3277 1.83 5.37 11.78 21.46 33.8 49.78 68.12 89.35 112.78];
+%     %scale = 1e2, synthetic
+%     sum_time_seq_OGD = 100:100:1000;
+%     sum_f_seq_OGD = [0.3277 1.83 5.37 11.78 21.46 33.8 49.78 68.12 89.35 112.78];
+%     
+%     sum_time_seq_OMGD = 100:100:1000;
+%     sum_f_seq_OMGD = [0.3222 1.77 5.11 11.02 19.66 30.28 43.46 57.82 73.49 89.63];
+%     
+%     sum_time_seq_MOGD = 100:100:1000;
+%     sum_f_seq_MOGD = [0.2967 1.513 4.0338 8.021 13.10 18.34 23.76 28.43 32.39 35.44];
+%     
+%     semilogy(sum_time_seq_OGD, sum_f_seq_OGD, '-r','LineWidth', 2);
+%     hold on;
+%     semilogy(sum_time_seq_OMGD, sum_f_seq_OMGD, '--b','LineWidth', 2);
+%     hold on;
+%     
+%     semilogy(sum_time_seq_MOGD, sum_f_seq_MOGD, ':k','LineWidth', 2);
+%     hold on;
+%scale = 1e2, real dataset: skin
+    sum_time_seq_OGD = 100:100:800;
+    sum_f_seq_OGD = [0.0311 0.0339 0.036 0.038 0.0387 0.0406 0.0408 0.0424];
     
-    sum_time_seq_OMGD = 100:100:1000;
-    sum_f_seq_OMGD = [0.3222 1.77 5.11 11.02 19.66 30.28 43.46 57.82 73.49 89.63];
+    sum_time_seq_OMGD = 100:100:800;
+    sum_f_seq_OMGD = [0.00619 0.0068 0.0072 0.0076 0.00786 0.0082 0.0084 0.0087];
     
-    sum_time_seq_MOGD = 100:100:1000;
-    sum_f_seq_MOGD = [0.2967 1.513 4.0338 8.021 13.10 18.34 23.76 28.43 32.39 35.44];
+    sum_time_seq_MOGD = 100:100:800;
+    sum_f_seq_MOGD = [0.001 0.0013 0.0014 0.0016 0.00175 0.00187 0.00206 0.002156];
     
     semilogy(sum_time_seq_OGD, sum_f_seq_OGD, '-r','LineWidth', 2);
     hold on;
@@ -27,7 +43,7 @@ if strcmp(model_opt,'logistic_regression')
     ylabel('Dynamic regret', 'fontsize', 18 );
     %set(gca,'ytick',[1e-9 1e-7 1e-5 1e-3 1e-1 1e1 1e3]);
     %set(gca,'yticklabel',{'Two','Four','Five','Seven'});
-    axis([100 1000 1 150]);
+    axis([100 800 1e-4 1e-1]);
     %$$ \mathcal{P}_T^{\ast} $$
     h = legend('OGD', ...
         'OMGD', ...
