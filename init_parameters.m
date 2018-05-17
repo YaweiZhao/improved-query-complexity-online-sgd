71ffc418179a5712cb6ced5226e945444108624e
Yawei Zhao <zhaoyawei@nudt.edu.cn>
2018-05-16 20:23:19 -0400

    dd
    dd

--- init_parameters.m
+++ init_parameters.m
@@ -17,7 +17,7 @@ s_hyp.d = 3;
 %s_hyp.d = 100;
 s_hyp.T = s_hyp.n;  
 s_hyp.kappa = 1e3;
-s_hyp.scale_D = 1e0;
+s_hyp.scale_D = 1e2;
 s_hyp = load_data( 'skin.mat', s_hyp);
 %s_hyp = generate_dynamic_data_stream(s_hyp);
 
@@ -31,7 +31,7 @@ s_hyp.ALGO = 'MOGD10';%our method
 %s_hyp.ALGO = 'MOGD-SVRG-BB';
 
 %model settings
-s_hyp.alpha = 1e-5;%regularization constant
+s_hyp.alpha = 1e-6;%regularization constant
 %s_hyp.eta = 1e-2/sqrt(s_hyp.T); % for stochastic setting
 s_hyp.eta = 1e-8;
 
--- online_optimization.m
+++ online_optimization.m
@@ -116,7 +116,7 @@ for i=1:T %n >> T
     %[x_seq(i,:), f_seq(i,:)] = get_local_minimizer(x_t, Ai, [],  s_hyp) ;
     %f_t_seq(i,:) = get_local_loss(x_t, Ai, [],  s_hyp) ;
     x_seq(i,:) = x_t';
-    interval=10;
+    interval=5;
     if strcmp(ALGO, 'MOGD10') || strcmp(ALGO, 'OMGD')
         if mod(i,interval) == 0
             counter = fix(i/interval);
@@ -127,7 +127,7 @@ for i=1:T %n >> T
             tic;
         end
     elseif strcmp(ALGO, 'OGD')
-        interval = interval*4; 
+        interval = interval*2; 
         if mod(i,interval) == 0
             counter = fix(i/interval);
             time_seq(counter,:) = toc;%i
@@ -139,7 +139,7 @@ for i=1:T %n >> T
     end
     %terminate the process
     %cpu_seconds = cpu_seconds + time_seq(i,:);
-    if sum(time_seq)>106%
+    if sum(time_seq)>206%
         break;
     end
     
